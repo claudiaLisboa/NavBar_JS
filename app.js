b68694b91d33070af284475b12f797044660460e
Claudia Lisboa <ccalumby@gmail.com>
2022-02-13 00:31:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/*Manipulate the DOM by building a simple JavaScript NavBar    
+For studding proposes
+
+classList => shows/gets all classes
+contains => checks classList for specific clas
+add => add class
+remove => remove class
+toggle => toggles class
+ */
